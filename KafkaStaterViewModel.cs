52bc1db4440bd2a0f565d55c2222015024423058
flason5bright <flason5bright@163.com>
2020-02-21 10:25:46 +0800

    Init project

---
+++ KafkaStaterViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace KafkaStarter
+{
+    public class KafkaStaterViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string zoKeeperConsole = "";
+        public string ZoKeeperConsole
+        {
+            get { return zoKeeperConsole; }
+            set
+            {
+                zoKeeperConsole = value;
+                NotifyPropertyChanged(nameof(ZoKeeperConsole));
+            }
+        }
+
+        public ICommand StartZokeeper { get { return new RelayCommand(StartZokeeperExcute); } }
+
+        private Process zoKeeperProcess;
+        private void StartZokeeperExcute()
+        {
+            if (zoKeeperProcess != null)
+                zoKeeperProcess.Close();
+            StringBuilder sb = new StringBuilder();
+            zoKeeperProcess = ProcessHelper.CreateProcess("ping 127.0.0.1", new System.Diagnostics.DataReceivedEventHandler((o, e) =>
+            {
+                ZoKeeperConsole = sb.Append(e.Data).Append("\r\n").ToString();
+            }));
+
+
+
+            //DispatcherTimer timer = new DispatcherTimer();
+            //timer.Interval = new TimeSpan(0, 0, 5);
+            //timer.Tick += new EventHandler((object sender, EventArgs e) =>
+            // {
+            //     ZoKeeperConsole = process.StandardOutput.ReadToEnd();
+            // });  
+            //timer.Start();
+
+        }
+
+    }
+}
---
+++ ProcessHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaStarter
+{
+    public class ProcessHelper
+    {
+
+        public static Process CreateProcess(string command, DataReceivedEventHandler handler)
+        {
+            try
+            {
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;//是否使用操作系统shell启动
+                p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+                p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+                p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+                p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+                p.OutputDataReceived += handler;
+                p.Start();//启动程序
+
+                string strCMD = command;
+                //向cmd窗口发送输入信息
+                p.StandardInput.WriteLine(strCMD + "&exit");
+
+                p.StandardInput.AutoFlush = true;
+
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+
+                //等待程序执行完退出进程
+                return p;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
